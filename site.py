da1172a36f3dfbaad5fca23ae600441d2b687e35
Bart <mail@bartsunter.nl>
2013-11-30 16:37:38 +0100

    add a dropdown menu to select the query type

--- site.py
+++ site.py
@@ -19,27 +19,28 @@ def index():
 @app.route('/recommend', methods=['POST', 'GET'])
 def recommend():
     query = request.json['query']
+    querytype = request.json['type']
     # do something with the query here; main recommending function
-    query = distance.getRecommendedAuthors(config.SUMMARIES,query)
+    # results = distance.getRecommendedAuthors(config.SUMMARIES,query)
     # return a list of recommended papers with e.g. scores and stuff
-    return jsonify({"query" : query})
+    return jsonify({"query" : query, "type" : querytype})
 
 if __name__ == '__main__':
-    print 'Loading data...'
-    t0 = time()
-    #Create a normal 0..X counted array, we don't use the id's anyway
-    Summaries = []
-    paper = namedtuple('paper', ['title', 'authors', 'year', 'doi'])
-    for (i, paper_info) in enumerate(dataloader.load(datasets_folder + 'summaries.pkl').values()):
-        Summaries.append(paper(*paper_info))
-    #Summaries is in format like ('Patterns of sex work contact among men in the general population of Switzerland, 1987-2000.', ['Jeannin A', 'Rousson V', 'Meystre-Agustoni G', 'Dubois-Arber F'], 2008, '10.1136/sti.2008.030031')
-    config.SUMMARIES = Summaries
-    config.IDS = dataloader.load(datasets_folder + 'ids.pkl')
-    config.CITATIONS = dataloader.load(datasets_folder + 'citations.pkl')
-    config.ABSTRACTS = dataloader.load(datasets_folder + 'abstracts.pkl')
-    config.KEYWORDS = dataloader.load(datasets_folder + 'keywords.pkl')
-    t1 = time()
-    print 'Loaded %d tuples in %fs' % (len(config.SUMMARIES) + len(config.IDS) + len(config.CITATIONS) + len(config.ABSTRACTS) + len(config.KEYWORDS), t1-t0)
+    # print 'Loading data...'
+    # t0 = time()
+    # #Create a normal 0..X counted array, we don't use the id's anyway
+    # Summaries = []
+    # paper = namedtuple('paper', ['title', 'authors', 'year', 'doi'])
+    # for (i, paper_info) in enumerate(dataloader.load(datasets_folder + 'summaries.pkl').values()):
+    #     Summaries.append(paper(*paper_info))
+    # #Summaries is in format like ('Patterns of sex work contact among men in the general population of Switzerland, 1987-2000.', ['Jeannin A', 'Rousson V', 'Meystre-Agustoni G', 'Dubois-Arber F'], 2008, '10.1136/sti.2008.030031')
+    # config.SUMMARIES = Summaries
+    # config.IDS = dataloader.load(datasets_folder + 'ids.pkl')
+    # config.CITATIONS = dataloader.load(datasets_folder + 'citations.pkl')
+    # config.ABSTRACTS = dataloader.load(datasets_folder + 'abstracts.pkl')
+    # config.KEYWORDS = dataloader.load(datasets_folder + 'keywords.pkl')
+    # t1 = time()
+    # print 'Loaded %d tuples in %fs' % (len(config.SUMMARIES) + len(config.IDS) + len(config.CITATIONS) + len(config.ABSTRACTS) + len(config.KEYWORDS), t1-t0)
 
     app.run()
 
--- templates/js/main.js
+++ templates/js/main.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
     $('.paper-searchbar--submit').on('click', function() {
+        var type = $('.paper-searchbar--type').val();
         var query = $('.paper-searchbar--input').val();
 
         $.ajax({
             type: "post",
             url: "http://localhost:5000/recommend",
-            data: '{ "query" : "' + query + '" }',
+            data: '{ "query" : "' + query + '", "type" : "' + type + '" }',
             contentType: "application/json; charset=utf-8",
             dataType: "json",
             success : function(data) {
-                alert(data.query);
+                alert(data.query + data.type);
             },
             error : function(data) {
                 alert("error");
