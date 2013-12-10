a79bd98a2c3487eb570df123970f1e84876c6f41
Nino Floris <ninofloris@gmail.com>
2013-12-10 22:14:50 +0100

    Redid pagerank with Baudouin, add Jari Fergusons stopwords set and an inversed citations set (references basically)

--- recommender/pagerank.py
+++ recommender/pagerank.py
@@ -1,49 +1,59 @@
+import collections
 
+def pagerank(paperCitations, paperCitedBy, minChanges=10, maxRounds=150):
+    """Calculates the pagerank for every citation of the papers we have citation data of.
 
-"""
-    This function return a vector as big as the number of papers, and for each paper of the vector contain a coefficient
-    The first parameter is the set with all the papers
-    The second parameter is a function telling for a certain paper all the papers that cites it
-    (Optional) The third argument is the minimum of changes required to said that a loop has been useful
-    (Optional) The fourth argument is te max rounds we like the algorithm to do
-"""
+    Keyword arguments:
+    paperCitations -- the dict containing all the papers and their citations
+    paperCitedBy -- the inversed dict of paperCitations
+    minChanges -- the minimum of changes required for the loop to continue (default 10)
+    maxRounds -- the max rounds pagerank should do, minChanges breaks out of this loop first (default 150)
 
-def pagerank(paperSet, paperCites, minChanges=2, maxRounds=150):
-    ranks = [0]*len(paperSet)  # Creating the answer array
-    changes = minChanges + 1 # Just to be sure to enter the while once
+    Returns -- tuple: (rounds=int, {pmid: pagerank})
+
+    """
+    ranks = {}  # Creating the answer array
+    changes = minChanges + 1  # Just to be  sure to enter the while once
     rounds = 0
-    avgRank = 1
+    avgRank = 1.0
     jumpProbability = 0.15
-    while (changes<minChanges & rounds < maxRounds):
-        for i in range (len(paperSet)):       # For each paper of the paperSet, we calculate a rank
-            citingPaper = paperCites(paperSet[i])     #Here, we get the citation from the paper i (in the paperSet Array
-            previousRank = ranks[i]
-            if (len(citingPaper) == 0) :              # If there is no citations from this paper, it get only the average rank
-                ranks[i] = avgRank
-            else :
-                ranks[i] = 0
-                for j in range(len(citingPaper)):
-                    ranks[i] = ranks[citingPaper[j][0]] * citingPaper[j][1] # WARNING HERE : the citing paper needs to be an array of tuples that contains :
-                                                                            # as first argument the paper ID (to retrieve the paper in the paperSet)
-                                                                            # as second argument the number of citations
-                ranks[i]*= (1-jumpProbability) + jumpProbability
 
-            if (previousRank - ranks[i]) > 0.1 :            # To be adapted if it is too strong or too light
-                changes += 1
-        avgRank = avgArray(ranks)
+    while (changes > minChanges and rounds < maxRounds):
+        changes = 0
+        for pmid, citations in paperCitations.iteritems():       # For each paper of the paperSet, we calculate a rank
+            if (len(citations) > 0):
+                oldRank = ranks.setdefault(pmid, avgRank)
+                rank = 0.0
+
+                for reference in paperCitedBy[pmid]:
+                    try:
+                        rank += (ranks[reference] / len(paperCitations[reference]))
+                    except KeyError:
+                        rank += avgRank / 3 # If this paper is outside the dataset use the avgRank divided by the supposed amount of cited papers.
+
+                ranks[pmid] = (1-jumpProbability) * rank + jumpProbability
+                if (oldRank - ranks[pmid]) > 0.01:            # To be adapted if it is too strong or too light
+                    changes += 1
+            else:
+                ranks[pmid] = avgRank
         rounds += 1
-    return ranks
+    return (rounds, ranks)
 
-"""
-    This function simply makes an average of an array passed as a parameter
-    The only argument is the array
-"""
 
-def avgArray(foo):
-    avg = 0
-    for i in range (len(foo)):
-        avg += foo[i]
-    avg /= len(foo)
-    return avg
+
+def citedBy(paperCitations):
+    """Creates the inverse of the citation dataset
+
+        Keyword arguments:
+        paperCitations -- the dict containing all the papers and their citations
+
+        Returns -- {pmid: [references]}
+        
+    """
+    cited_by = collections.defaultdict(list)
+    for ref, papers_citing_ref in paperCitations.iteritems():
+        for pmid in papers_citing_ref:
+            cited_by[pmid].append(ref)
+    return cited_by
 
 
