4092b0df99222276a953d96493cc9de498add114
Baudouin Duthoit <baud.duthoit@gmail.com>
2013-12-11 01:04:55 +0100

    Update of clustering.py (NOT COMPLETELY DONE YET) Update of comments on pagerank.py Keep your tests in the test.py (they are a kin of a proof for our algorithms) Cheers ! :)

--- recommender/tests.py
+++ recommender/tests.py
@@ -6,13 +6,26 @@ import config
 from time import time
 from helpers import timeRun
 
-
+""" Loading for tests """
 
 t0 = time()
 config.ABSTRACTS = cPickle.load(open("../datasets/abstracts.pkl", 'rb'))
 config.CITATIONS = cPickle.load(open("../datasets/citations.pkl", 'rb'))
 t1 = time()
 print 'Loaded %d tuples in %fs' % (len(config.ABSTRACTS), t1-t0)
+
+
+""" Clustering Tests """
+
+clusterPoints1 = clustering.createFirstCentroid(config.CITATIONS,5)
+
+def dist(a,b):
+    return a+b/3
+
+print(clusterPoints1)
+#print(clustering.updatecentroids(clusterPoints1, dist, 5))
+
+""" Pagerank Tests """
 
 #cPickle.dump(pagerank.citedBy(config.CITATIONS), open("../datasets/citedby.pkl", 'wb'), cPickle.HIGHEST_PROTOCOL)
 #print "Pickled citedby"
