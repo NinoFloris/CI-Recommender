e839da6bc6f03d993636a9477330a62b44fa588a
leonbenjamin15 <leonbenjamin15@hotmail.com>
2013-12-11 17:23:07 +0100

    Update searches.py
    added functions, searches should become the connector between the flask interface and the algorithms. Right now searchTopXterm can use the TFIDF dictionary as input.

--- recommender/searches.py
+++ recommender/searches.py
@@ -1,16 +1,16 @@
 import math
 import config
-import TFIDF
 from numpy import *
 
 #abstDict = TFIDF.TFIDF(config.ABSTRACTS)
-
 abstDict = {1 : [('la', 1), ('bo', 1), ('si', 1)],
-            2: [('ra', 1), ('la', 1), ('bo', 1)],
+            2: [('ra', 1), ('la', 2), ('bo', 1)],
             3: [('xz', 1), ('ry', 1), ('der', 1)],
-            4: [('si', 1), ('tu', 1), ('ta', 1)]}
+            4: [('si', 1), ('la', 3), ('ta', 1)]}
 
-def searchTopXterms(x, paperID): #parameters are integers 
+Summaries = config.SUMMARIES
+
+def searchTopXterms(x, paperID): #this function recommends papers on paper input based on TFIDF output
     results = [[]] * (x+1)
     terms = []
     for i in range(0, x):
@@ -28,14 +28,30 @@ def searchTopXterms(x, paperID): #parameters are integers
         results[count] = results[count] + [pID]
     return results #returns list of pID tuples. Index in list is n of matches
 
-def searchTerm(term):  # searches TFIDF dict for term matches
+def searchTermTitle(term):  # searches Summaries for term matches
+    results = []
+    for paper in config.SUMMARIES:
+        if term in paper[0]: #is term in title?
+            results.append(paper[0]);
+    return results
+
+def searchAuthor(author):
+    results = []
+    for paper in config.SUMMARIES
+        if author in paper[1]:
+            results.append(paper)
+    return results
+
+def searchTermTFIDF(term):
     results = []
     for pID in abstDict:
         for item in abstDict[pID]:
-            if term in item:
-                results.append(pID)        
-    return results
+            for termScore in item:
+                if term in termScore:
+                    results.append((termscore[1], Summaries[pID]))
+    return sorted(results, key=lambda tup: tup[0])   #sort based on first element
 
-print searchTerm('la');
+
+#print searchTermTitle('virus');
 #print searchTopXterms(3, 1)
     
