e4a7102461256a6a959de64000481dc4fdd6c180
Nino Floris <ninofloris@gmail.com>
2013-12-22 18:42:06 +0100

    pruned distance

--- recommender/distance.py
+++ recommender/distance.py
@@ -6,63 +6,21 @@ from itertools import groupby
 import clustering, config
 from math import sqrt
 
-# Returns the Pearson correlation coefficient for p1 and p2
-def getPearson(prefs,p1,p2):
-    # Get the list of mutually rated items
-    si={}
-    for item in prefs[p1]: 
-      if item in prefs[p2]: si[item]=1
-
-    # if they are no ratings in common, return 0
-    if len(si)==0: return 0
-
-    # Sum calculations
-    n=len(si)
-
-    # Sums of all the preferences
-    sum1=sum([prefs[p1][it] for it in si])
-    sum2=sum([prefs[p2][it] for it in si])
-
+def pearson(v1,v2):
+    # Simple sums
+    sum1=sum(v1)
+    sum2=sum(v2)
     # Sums of the squares
-    sum1Sq=sum([pow(prefs[p1][it],2) for it in si])
-    sum2Sq=sum([pow(prefs[p2][it],2) for it in si])	
-
+    sum1Sq=sum([pow(v,2) for v in v1])
+    sum2Sq=sum([pow(v,2) for v in v2])
     # Sum of the products
-    pSum=sum([prefs[p1][it]*prefs[p2][it] for it in si])
-
+    pSum=sum([v1[i]*v2[i] for i in range(len(v1))])
     # Calculate r (Pearson score)
-    num=pSum-(sum1*sum2/n)
-    den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+    num=pSum-(sum1*sum2/len(v1))
+    den=sqrt((sum1Sq-pow(sum1,2)/len(v1))*(sum2Sq-pow(sum2,2)/len(v1)))
     if den==0: return 0
+    return 1.0-num/den
 
-    r=num/den
-
-    return r    
-
-def getPapers(pID):
-    print "starting getpapers...\n"
-    abstractWords = getwords(config.ABSTRACTS[pID])    
-    titleWords = getwords(config.RAWSUMMARIES[pID][0]) #title
-    for paperID in config.RAWSUMMARIES:
-        score = wordMatch(titleWords, getwords(config.RAWSUMMARIES[paperID][0]))    # compare based on title
-        if score > 3:
-            print "\nMatch found:"
-            print config.RAWSUMMARIES[paperID][0]
-
-def termSearch(term):
-    for paperID in config.ABSTRACTS:
-        abstractWords = getwords(config.ABSTRACTS[paperID])
-        for word in abstractWords:
-            if word == term:
-                print "\nMatch found:"
-                print config.RAWSUMMARIES[paperID][0]
-
-def topMatches(prefs,person,n=5,similarity=getPearson):
-    scores=[(similarity(prefs,person,other),other)for other in prefs if other!=person]
-    # Sort the list so the highest scores appear at the top
-    scores.sort()
-    scores.reverse()
-    return scores[0:n]
 
 def getDistances(data, v1):
     distancelist=[]
@@ -97,41 +55,6 @@ def transformDict(data):
             result[val][key] = 0.0
     return result
 
-#deprecated, use Counter(document.split()) or TF(document.split())
-def getWords(doc):
-    splitter = re.compile('\\W*')
-    words = [s.lower() for s in splitter.split(doc)  if len(s) > 4 and len(s) < 20]
-    dict = {}
-    for w in words:
-        dict[w] = words.count(w)
-    return dict
 
-def getRecommendedAuthors(itemMatch, author_name):
-    userRatings = []   
-    for i, item in enumerate(config.SUMMARIES):
-        if author_name in config.SUMMARIES[i].authors:
-            userRatings.append(config.SUMMARIES[i].authors)
 
-    for item in userRatings:
-        print item
 
-    scores={}
-    totalSim={}
-    # Loop over items rated by this user
-    for (item,rating) in userRatings:
-    # Loop over items similar to this one
-        for (similarity,item2) in itemMatch[item]:
-            # Ignore if this user has already rated this item
-            if item2 in userRatings: continue
-            # Weighted sum of rating times similarity
-            scores.setdefault(item2,0)
-            scores[item2]+=similarity*rating
-            # Sum of all the similarities
-            totalSim.setdefault(item2,0)
-            totalSim[item2]+=similarity
-    # Divide each total score by total weighting to get an average
-    rankings=[(score/totalSim[item],item) for item,score in scores.items( )]
-    # Return the rankings from highest to lowest
-    rankings.sort( )
-    rankings.reverse( )
-    return rankings
