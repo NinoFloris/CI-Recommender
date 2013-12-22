55a43ca2684eaa7318e20edc0c56897b0e97b296
Nino Floris <ninofloris@gmail.com>
2013-12-22 16:11:44 +0100

    new searches function

--- recommender/searches.py
+++ recommender/searches.py
@@ -1,6 +1,7 @@
-import math
-import config
-from numpy import *
+import collections
+
+import datasets
+from helpers import sliceDict
 
 #abstDict = TFIDF.TFIDF(config.ABSTRACTS)
 abstDict = {1 : [('la', 1), ('bo', 1), ('si', 1)],
@@ -8,48 +9,45 @@ abstDict = {1 : [('la', 1), ('bo', 1), ('si', 1)],
             3: [('xz', 1), ('ry', 1), ('der', 1)],
             4: [('si', 1), ('la', 3), ('ta', 1)]}
 
-def searchTopXterms(x, paperID): #this function recommends papers on paper input based on TFIDF output
-    results = [[]] * (x+1)
-    terms = []
-    for i in range(0, x):
-        terms.append(abstDict[paperID][i][0])
-    print terms
-    print "\n"
-    for pID in abstDict:
-        count = 0
-        for i in range(0, x):
-            print results
-            if abstDict[pID][i][0] in terms:
-                count += 1
-                print abstDict[pID][i][0]
-                print "in terms \n"
-        results[count] = results[count] + [pID]
-    return results #returns list of pID tuples. Index in list is n of matches
+def query(query, subSet, useIndependentFeatures, usePageRank, useTFIDF, useClustering, useRecommender, topN=100):
+    results = {}
+    prevMethod = ''
 
-def searchTermTitle(term):  # searches Summaries for term matches
-    results = []
-    for pmid, paper in config.SUMMARIES.iteritems():
-        if term in paper.title: #is term in title?
-            results.append(pmid)
-    return results
+    if not useIndependentFeatures and not usePageRank and not useTFIDF and not useClustering and not useRecommender:
+        return None
 
-def searchAuthor(author):
-    results = []
-    for pmid, paper in config.SUMMARIES.iteritems():
-        if author in paper.authors:
-            results.append(pmid)
-    return results
+    query = normalizeString(query, datasets.STOPWORDS, lemmatization).split()
+    print "Querying with: %r" % query
 
-def searchTermTFIDF(term):
-    results = []
-    for pID in abstDict.iterkeys():
-        for item in abstDict[pID]:
-            for termScore in item:
-                if term in termScore:
-                    results.append((termscore[1], Summaries[pID]))
-    return sorted(results, key=lambda tup: tup[0])   #sort based on first element
+    if useIndependentFeatures:
+        results['IF'] = queryIndependentFeatures(query, results[prevMethod].keys(), subSet)
+        prevMethod = 'IF'
+    if usePagerank:
+        results['PR'] = queryPageRank(query, results[prevMethod].keys(), subSet)
+        prevMethod = 'PR'
+    if useTFIDF:
+        results['TI'] = queryTFIDF(query, results[prevMethod].keys(), subSet)
+        prevMethod = 'TI'
+    if useClustering:
+        results['CL'] = queryClustering(query, results[prevMethod].keys(), subSet)
+        prevMethod = 'CL'
+    if useRecommender:
+        results['RE'] = queryRecommender(query, results[prevMethod].keys(), subSet)
+        prevMethod = 'RE'
 
+    endresults = collections.defaultdict(float)
+    # Loop through all pmid's after having ID set sliced like subSet prescribes
+    for pmid in sliceDict(datasets.IDS, subSet).iterkeys():
+        endresults[pmid] += results.setdefault('IF',{}).setdefault(pmid,0.0) * 0.0 # Weighted modifier, if required
+        endresults[pmid] += results.setdefault('PR',{}).setdefault(pmid,0.0) * 0.0 # Weighted modifier, if required
+        endresults[pmid] += results.setdefault('TI',{}).setdefault(pmid,0.0) * 0.0 # Weighted modifier, if required
+        endresults[pmid] += results.setdefault('CL',{}).setdefault(pmid,0.0) * 0.0 # Weighted modifier, if required
+        endresults[pmid] += results.setdefault('RE',{}).setdefault(pmid,0.0) * 0.0 # Weighted modifier, if required
+        endresults[pmid] /= len(results)
 
-#print searchTermTitle('virus');
-#print searchTopXterms(3, 1)
-    
+    # Return the topN results, sorted descending by score
+    return sorted(endresults.items(), key=operator.itemgetter(1), reverse=True)[:topN]
+
+def recommend(query, subSet, useIndependentFeatures, usePageRank, useTFIDF, useClustering, useRecommender, topN=100):
+    pass
+    # requires user to select similar cluster, or do it for the user. so not taking the most similar to query but 2nd similar
